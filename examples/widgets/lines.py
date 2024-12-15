cfc5a442243584dc3684f7f67e4f445813efea74
Tong Zhang <zhangt@frib.msu.edu>
2024-12-15 14:34:57 -0500

    REF: Add how to set arrows to a Line widget.
    - The simplest way to draw an arrow.

--- examples/widgets/lines.py
+++ examples/widgets/lines.py
@@ -3,6 +3,8 @@ from opigen.contrib import Display, VerticalLine
 from opigen.contrib import HorizontalLine
 from opigen.opimodel.widgets import LineStyles
 from itertools import cycle
+from opigen import colors
+from opigen.opimodel.utils import filter_attributes
 
 
 def main():
@@ -12,11 +14,15 @@ def main():
     thickness = 2
     length = 400
     line_styles = cycle(LineStyles)
+    phoebus_colors = cycle(filter_attributes(colors, "PHOEBUS_*").values())
     # H lines
     for i in range(10):
         line = HorizontalLine(x0, y0, length, thickness=thickness, style=next(line_styles))
+        line.set_line_color(next(phoebus_colors))
+        line.set_arrow_style("to")
+        line.set_arrow_length(10)
         y0 += 15
-        thickness += 1
+        thickness += 0.5
         screen.add_child(line)
 
     # V lines
@@ -24,8 +30,11 @@ def main():
     thickness = 2
     for i in range(10):
         line = VerticalLine(x0, y0, length, thickness=thickness, style=next(line_styles))
+        line.set_line_color(next(phoebus_colors))
+        line.set_arrow_style("from")
+        line.set_arrow_length(10)
         x0 += 15
-        thickness += 1
+        thickness += 0.5
         screen.add_child(line)
 
     #
