61cba13a0b687dee8e048090f088cec4014043a7
MihirNaik05 <naikmihi@msu.edu>
2026-06-02 14:49:46 -0400

    ENH: added open flag to automate opening of phoebus

--- examples/widgets/slider.py
+++ examples/widgets/slider.py
@@ -1,17 +1,31 @@
 from opigen import Renderer
 from opigen.widgets import ScaledSlider 
 from opigen.contrib import Display 
+# faster opening  
+import argparse
+import subprocess
 
-slider = ScaledSlider(x=40, y=40, 
-                      width=400,height=100,
-                      pv_name="loc://slider(0)",# define local variable for CS studio
-                      limits_from_pv=True # inherit value limits from the process var 
-                      ) 
+def main():
+    slider = ScaledSlider(x=40, y=40, 
+                        width=400,height=100,
+                        pv_name="loc://slider(0)",# define local variable for CS studio
+                        limits_from_pv=True # inherit value limits from the process var 
+                        ) 
 
-screen = Display(1200,800,name="Slider Widget")
+    screen = Display(1200,800,name="Slider Widget")
 
-screen.add_child(slider)
+    screen.add_child(slider)
 
-r = Renderer(screen, auto_resize=True)
-r.to_bob("examples/widgets/slider.bob")  
+    r = Renderer(screen, auto_resize=True)
+    r.to_bob("examples/widgets/slider.bob")  
 
+if __name__=="__main__":
+    main()
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-open', action='store_true')
+    OPEN = parser.parse_args().open
+    if OPEN:
+        print("Opening Phoebus...")
+        subprocess.Popen(['/usr/bin/phoebus', "-resource", 'examples/widgets/slider.bob'])
